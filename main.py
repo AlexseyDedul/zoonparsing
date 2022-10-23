b5e9bbc081c9844d78ec29b23b851d2585b26e4f
Алексей Дедюль <dedul.an@prj365.com>
2022-10-23 19:33:59 +0300

    zoon parsing project

--- main.py
+++ main.py
@@ -3,20 +3,20 @@
 import pandas as pd
 from parser import pars
 
-# dfCatalogMinsk = pd.DataFrame(pars('https://zoon.by/minsk/'))
-# dfCatalogMinsk.to_excel('./catalogMinsk.xlsx')
+dfCatalogMinsk = pd.DataFrame(pars('https://zoon.by/minsk/'))
+dfCatalogMinsk.to_excel('./catalogMinsk.xlsx')
 
-# dfCatalogMogilev = pd.DataFrame(pars('https://zoon.by/mogilev/'))
-# dfCatalogMogilev.to_excel('./catalogMogilev.xlsx')
-#
-# dfCatalogBrest = pd.DataFrame(pars('https://zoon.by/brest/'))
-# dfCatalogBrest.to_excel('./catalogBrest.xlsx')
-#
-# dfCatalogGomel = pd.DataFrame(pars('https://zoon.by/gomel/'))
-# dfCatalogGomel.to_excel('./catalogGomel.xlsx')
-#
-# dfCatalogGrodno = pd.DataFrame(pars('https://zoon.by/grodno/'))
-# dfCatalogGrodno.to_excel('./catalogGrodno.xlsx')
-#
+dfCatalogMogilev = pd.DataFrame(pars('https://zoon.by/mogilev/'))
+dfCatalogMogilev.to_excel('./catalogMogilev.xlsx')
+
+dfCatalogBrest = pd.DataFrame(pars('https://zoon.by/brest/'))
+dfCatalogBrest.to_excel('./catalogBrest.xlsx')
+
+dfCatalogGomel = pd.DataFrame(pars('https://zoon.by/gomel/'))
+dfCatalogGomel.to_excel('./catalogGomel.xlsx')
+
+dfCatalogGrodno = pd.DataFrame(pars('https://zoon.by/grodno/'))
+dfCatalogGrodno.to_excel('./catalogGrodno.xlsx')
+
 dfCatalogVitebsk = pd.DataFrame(pars('https://zoon.by/vitebsk/'))
 dfCatalogVitebsk.to_excel('./catalogVitebsk.xlsx')
